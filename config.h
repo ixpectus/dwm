cb3f549bb57294e2ac60a4d2bbfd2ed79f21d2fc
ixpectus <ixpectus@gmail.com>
2023-11-01 10:16:04 +0300

    DBAAS-main update configs

--- config.h
+++ config.h
@@ -9,15 +9,15 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { 
-  "MesloLGS Nerd Font:style=Regular:pixelsize=33:antialias=true:autohint=true",
-  "EmojiOne:style=Regular:pixelsize=33:antialias=true:autohint=true",
-};
+// static const char *fonts[]          = { 
+//   "MesloLGS Nerd Font:style=Regular:pixelsize=33:antialias=true:autohint=true",
+//   "EmojiOne:style=Regular:pixelsize=33:antialias=true:autohint=true",
+// };
 
-/* static const char *fonts[]          = { */ 
-/*   "MesloLGS Nerd Font:style=Regular:pixelsize=22:antialias=true:autohint=true", */
-/*   "EmojiOne:style=Regular:pixelsize=22:antialias=true:autohint=true", */
-/* }; */
+static const char *fonts[]          = {  
+  "MesloLGS Nerd Font:style=Regular:pixelsize=16:antialias=true:autohint=true", 
+  "EmojiOne:style=Regular:pixelsize=16:antialias=true:autohint=true",
+}; 
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
